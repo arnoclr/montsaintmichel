585e0da8cd3ddc64373e25e16fb92cf59c065f57
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-02-17 21:14:41 +0100

    ajout easing visite virtuelle

--- pages/vr.php
+++ pages/vr.php
@@ -22,6 +22,7 @@
     const BASE_URL = "https://cdn.arnocellarier.fr/s/iut/msm/vv/";
     const IMAGES_NUMBER = 361;
     const mainImg = document.querySelector('.js-vr-3D-img');
+    const IMAGES_GAP = 30; // nombre d'images entre 2 lieux
 
     // preload images
     const images = [];
@@ -31,22 +32,20 @@
         images.push(img);
     }
 
-    var i = 0;
-    var slidesToPass = 30;
-    var stopNextSlide;
-
-    const nextSlide = () => {
-        mainImg.src = images[i].src;
-        i++;
-        slidesToPass--;
-
-        if(slidesToPass == 0) {
-            slidesToPass = 30;
-            clearInterval(stopNextSlide);
-        }
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
     }
 
-    const nextLocation = () => {
-        stopNextSlide = setInterval(nextSlide, 1000/24);
+    let currentLocation = 0;
+
+    const nextLocation = async () => {
+        for (let i = 0; i <= IMAGES_GAP; i++) {
+            let r = i/IMAGES_GAP;
+            let diffToMiddle = Math.abs(r - 0.5) * 2.3;
+            let ms = (1000/IMAGES_GAP /2) * ((diffToMiddle + 0.5)**2);
+            await sleep(ms);
+            mainImg.src = BASE_URL + `photo (${currentLocation*IMAGES_GAP + i}).webp`;
+        }
+        currentLocation++;
     }
 </script>
