f039175c3e6972559599a9257c018218f79f94b5
xLeay <LarkyRiver@gmail.com>
2022-02-18 20:48:52 +0100

    branche et page php village

---
+++ pages/village/village.php
@@ -0,0 +1,11 @@
+<?php include "../../includes/head.php"; ?>
+
+<link rel="stylesheet" href="/src/styles/pages/village/village.css?v=<?= md5(filemtime($_SERVER['DOCUMENT_ROOT'] . '/src/styles/pages/index.css')) ?>">
+
+<?php include "../../includes/components/navbar.php"; ?>
+
+<div class="village">
+
+</div>
+
+<?php include "../../includes/components/footer.php"; ?>
