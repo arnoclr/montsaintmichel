bee49de780c201826f9ced88a91c9f24625ca98e
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-04-24 14:41:49 +0200

    language par défaut du navigateur

--- includes/functions/translations.php
+++ includes/functions/translations.php
@@ -2,19 +2,28 @@
 
 const COOKIE_LANG = "__hl";
 const GET_LANG_PARAM = "hl";
+const SUPPORTED_LANGAGES = ["fr", "en", "zh"];
 
 if (isset($_GET[GET_LANG_PARAM]) && !empty($_GET[GET_LANG_PARAM])) {
     $lang = $_GET[GET_LANG_PARAM];
     setcookie(COOKIE_LANG, $lang, time() + (86400 * 365), "/"); // 1 year
 } else {
-    $lang = isset($_COOKIE[COOKIE_LANG]) ? $_COOKIE[COOKIE_LANG] : 'fr';
+    $lang = 'fr';
+    $navigator_langage = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+    if (in_array($navigator_langage, SUPPORTED_LANGAGES)) {
+        $lang = $navigator_langage;
+    }
+
+    if (isset($_COOKIE[COOKIE_LANG]) && !empty($_COOKIE[COOKIE_LANG])) {
+        $lang = $_COOKIE[COOKIE_LANG];
+    }
 }
 
 function t($translation_id) {
     global $pdo, $lang;
     $stmt = $pdo->prepare('SELECT * FROM traductions WHERE id = :id LIMIT 1');
     $stmt->execute([':id' => $translation_id]);
-    // TODO: retourner dans la langue chioisie par l'utilisateur / ou la langue par défaut du navigateur
     return $stmt->fetch()->$lang ?? $translation_id;
 }
 
