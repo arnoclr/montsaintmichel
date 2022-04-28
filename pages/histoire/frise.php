f6e8fe2664b64b1c929c32220feec4d8c900943c
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-04-28 23:38:24 +0200

    ajout scroll contenu

--- pages/histoire/frise.php
+++ pages/histoire/frise.php
@@ -1,21 +1,40 @@
 <main class="frise">
 
-    <ul class="frise__timeline">
-        <li class="frise__timeline-date frise__timeline-date--active">700</li>
-        <li class="frise__timeline-date">800</li>
-        <li class="frise__timeline-date">900</li>
-        <li class="frise__timeline-date">1000</li>
-        <li class="frise__timeline-date">1100</li>
-        <li class="frise__timeline-date">1200</li>
-        <li class="frise__timeline-date">1300</li>
-        <li class="frise__timeline-date">1400</li>
-        <li class="frise__timeline-date">1500</li>
-        <li class="frise__timeline-date">1600</li>
-        <li class="frise__timeline-date">1700</li>
-        <li class="frise__timeline-date">1800</li>
-        <li class="frise__timeline-date">1900</li>
-        <li class="frise__timeline-date">2000</li>
-    </ul>
-    <div class="frise__timeline-line"></div>
+    <div class="frise__header">
+        <ul class="frise__timeline frise__timeline--date">
+            <li class="frise__timeline-date frise__timeline-date--active">700</li>
+            <li class="frise__timeline-date">800</li>
+            <li class="frise__timeline-date">900</li>
+            <li class="frise__timeline-date">1000</li>
+            <li class="frise__timeline-date">1100</li>
+            <li class="frise__timeline-date">1200</li>
+            <li class="frise__timeline-date">1300</li>
+            <li class="frise__timeline-date">1400</li>
+            <li class="frise__timeline-date">1500</li>
+            <li class="frise__timeline-date">1600</li>
+            <li class="frise__timeline-date">1700</li>
+            <li class="frise__timeline-date">1800</li>
+            <li class="frise__timeline-date">1900</li>
+            <li class="frise__timeline-date">2000</li>
+        </ul>
+        <div class="frise__timeline-line"></div>
+    </div>
+
+    <div class="frise__timeline frise__timeline--content">
+        <?php for ($i = 0; $i < 10; $i++) : ?>
+            <div class="frise__content">
+                <?php for ($j = 0; $j < 10; $j++) : ?>
+                    <span class="frise__content-date">708</span>
+                    <div class="frise__content-card">
+                        <img src="https://i.imgur.com/58Iltu3.jpg" alt="" class="frise__content-card-img">
+                        <div class="frise__content-card-text">
+                            <h3>Un titre</h3>
+                            <p>l’évêque Aubert fait ériger sur ce Mont un Sanctuaire, en l’honneur de l’Archange Saint-Michel (qui, d’après la légende, lui serait apparu et lui aurait ordonné de le faire). Le crâne de l’abbé Aubert repose aujourd’hui dans la basilique d'Avranches. Ce sanctuaire avait en fait la forme d’une petite grotte, qui ne peut contenir qu’une centaine de fidèles.</p>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        <?php endfor; ?>
+    </div>
         
 </main>
