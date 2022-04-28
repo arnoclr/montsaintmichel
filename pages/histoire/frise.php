3abcba15b9f0d4618363b11812791091eb2499df
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-04-28 22:46:51 +0200

    ajout timeline avec les dates

--- pages/histoire/frise.php
+++ pages/histoire/frise.php
@@ -1,3 +1,21 @@
 <main class="frise">
-    
+
+    <ul class="frise__timeline">
+        <li class="frise__timeline-date frise__timeline-date--active">700</li>
+        <li class="frise__timeline-date">800</li>
+        <li class="frise__timeline-date">900</li>
+        <li class="frise__timeline-date">1000</li>
+        <li class="frise__timeline-date">1100</li>
+        <li class="frise__timeline-date">1200</li>
+        <li class="frise__timeline-date">1300</li>
+        <li class="frise__timeline-date">1400</li>
+        <li class="frise__timeline-date">1500</li>
+        <li class="frise__timeline-date">1600</li>
+        <li class="frise__timeline-date">1700</li>
+        <li class="frise__timeline-date">1800</li>
+        <li class="frise__timeline-date">1900</li>
+        <li class="frise__timeline-date">2000</li>
+    </ul>
+    <div class="frise__timeline-line"></div>
+        
 </main>
