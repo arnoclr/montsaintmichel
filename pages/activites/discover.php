9bfc2803ddc3dc4c3551a18907e643bf61863f31
xLeay <LarkyRiver@gmail.com>
2022-05-17 02:17:18 +0200

    ajout: favicon (je sais pas comment on l'avait toujours pas mis mdr)

--- pages/activites/discover.php
+++ pages/activites/discover.php
@@ -1,17 +1,17 @@
 <?php
-include "./includes/components/navbar.php"; 
+include "./includes/components/navbar.php";
 ?>
 
 <link rel="stylesheet" href="../../src/styles/pages/activites/discover.css">
 
 <main>
     <div>
-    <img class="baie_haut" src="<?= i('activites\baie.png', 'medium') ?>" alt="">
-    <a href="/map">
-        <div class="button_map">
-    <button>click on</button>
-        </div> 
-    </a>
+        <img class="baie_haut" src="<?= i('activites\baie.png', 'medium') ?>" alt="">
+        <a href="/map">
+            <div class="button_map">
+                <button>click on</button>
+            </div>
+        </a>
     </div>
 </main>
 
