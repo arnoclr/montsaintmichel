079267f166e17f87fe4af522094ae1e9a3f361f1
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-05-19 08:24:53 +0200

    up: ajout commentaires pour générer des liens plus durables

--- includes/functions/accessAuthorization.php
+++ includes/functions/accessAuthorization.php
@@ -1,10 +1,14 @@
 <?php
 
 $nextDay = time() + (24 * 60 * 60);
+$nextMonth = time() + (30 * 24 * 60 * 60);
 
 $secret = '@nzfieoa67Cznuoicxzo';
 
 // die("?expires_at=$nextDay&signature=" . hash_hmac('sha256', $nextDay, $secret));
+// die("?expires_at=$nextMonth&signature=" . hash_hmac('sha256', $nextMonth, $secret));
+
+// die (date('Y-m-d h:i:s', $_GET['expires_at']));
 
 if (isset($_GET['expires_at']) && isset($_GET['signature'])) {
     $expires_at = $_GET['expires_at'];
