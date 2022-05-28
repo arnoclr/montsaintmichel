779675760ba8c6a62e62264483ad05bf69e26d2e
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-05-28 13:42:34 +0200

    feat: api d'auto complétion

--- includes/ajax/search.php
+++ includes/ajax/search.php
@@ -1,8 +1,15 @@
 <?php
 
-$terms = $_GET['q'] ?? "";
-
-$response = getPageFromSearchTerms($terms);
+switch ($_GET['action'] ?? "") {
+    case 'autoComplete':
+        $word = $_GET['word'] ?? "";
+        $response = getAutoCompletion($word);
+        break;
+    default:
+        $terms = $_GET['q'] ?? "";
+        $response = getPageFromSearchTerms($terms);
+        break;
+}
 
 header('Content-Type: application/json');
 echo json_encode($response);
--- includes/functions/search.php
+++ includes/functions/search.php
@@ -20,7 +20,10 @@ function getPageFromSearchTerms($q)
     $lang = lang();
 
     foreach ($terms as $term) {
-        echo lang();
+        if (preg_match('/^[a-z-]{0,3}$/i', $term)) {
+            continue;
+        }
+
         $req = $pdo->prepare("SELECT * FROM traductions WHERE ${lang} LIKE ?");
         $req->execute([
             '%' . $term . '%'
@@ -28,7 +31,13 @@ function getPageFromSearchTerms($q)
         $keys = array_merge($keys, $req->fetchAll(PDO::FETCH_COLUMN));
     }
 
-    $relevance = array_count_values($keys);
+    $filteredKeys = [];
+
+    foreach ($keys as $key) {
+        $filteredKeys[] = explode(".", $key)[0];
+    }
+
+    $relevance = array_count_values($filteredKeys);
 
     // order array by values
     arsort($relevance);
@@ -40,8 +49,39 @@ function getPageFromSearchTerms($q)
 
     foreach ($relevance as $key => $score) {
         $id = explode(".", $key)[0];
-        if ($id && $keyPages->$id) {
+        if ($id && isset($keyPages->$id)) {
             $response[] = $keyPages->$id;
+        }
+    }
+
+    return $response;
+}
+
+function getAutoCompletion($word)
+{
+    global $pdo;
+    $lang = lang();
+    $req = $pdo->prepare("SELECT ${lang} FROM traductions WHERE id NOT LIKE '%.keywords' AND ${lang} LIKE ? LIMIT 3");
+    $req->execute([
+        '%' . $word . '%'
+    ]);
+    $sentences = $req->fetchAll(PDO::FETCH_COLUMN);
+
+    // start setences with the word
+    $response = [];
+    foreach ($sentences as $sentence) {
+        $nextWords = substr($sentence, stripos($sentence, $word) + strlen($word));
+        $nextWords = str_replace(".", "", $nextWords);
+        $nextWords = str_replace(",", "", $nextWords);
+        $nextWords = explode(" ", $nextWords);
+        $nextWords = array_slice($nextWords, 0, 3);
+
+
+        $nextWords = implode(" ", $nextWords);
+        if (strlen($nextWords) > 2) {
+            if (!in_array($nextWords, $response)) {
+                $response[] = $nextWords;
+            }
         }
     }
 
