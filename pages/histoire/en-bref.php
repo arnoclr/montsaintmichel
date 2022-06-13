9f4ec2c673e0307fa9456aa368a825c2abc80265
Arno Cellarier <mrfeyurikenpro@gmail.com>
2022-06-13 10:23:48 +0200

    refactor: fin structure nouvelle page histoire

--- pages/histoire/en-bref.php
+++ pages/histoire/en-bref.php
@@ -4,29 +4,51 @@
 <?= t('nutshell.whyMsm.answer') ?>
 
 <img src="<?= i('nutshell/mont tombe.jpg') ?>" alt="Page d'un livre">
-<img src="<?= i('nutshell/construction.jpg') ?>" alt="Construction du Mont">
 <img src="<?= i('nutshell/charlemagne.jpg') ?>" alt="Portrait de Charlemagne">
+<img src="<?= i('nutshell/parallax/1783.jpg') ?>" alt="Baie du Mont Saint-Michel en 1783">
 
 <?= t('nutshell.whenBuilt.question') ?>
 <?= t('nutshell.whenBuilt.answer') ?>
 <?= t('nutshell.whenBuilt.answer.more') ?>
 
+<img src="<?= i('nutshell/notre dame sous terre.jpg') ?>" alt="Notre dame sous terre">
+<img src="<?= i('nutshell/incendie.jpg') ?>" alt="Incendie">
+
 <?= t('nutshell.whyBuilt.question') ?>
 <?= t('nutshell.whyBuilt.answer') ?>
 <?= t('nutshell.whyBuilt.answer.more') ?>
+
+<img src="<?= i('nutshell/archange saint michel.jpg') ?>" alt="Archange Saint Michel">
+<img src="<?= i('nutshell/eveque.jpg') ?>" alt="Eveque">
+<img src="<?= i('nutshell/vikings.jpg') ?>" alt="Invasion de vikings par la mer">
+<img src="<?= i('nutshell/christianisme.jpg') ?>" alt="Representation du Christianisme en dessin">
+<img src="<?= i('nutshell/parallax/christianisme.jpg') ?>" alt="Representation du Christianisme">
 
 <?= t('nutshell.whoBuilt.question') ?>
 <?= t('nutshell.whoBuilt.answer') ?>
 <?= t('nutshell.whoBuilt.answer.more') ?>
 
+<img src="<?= i('nutshell/construction.jpg') ?>" alt="Ouvriers en train de construire le Mont Saint-Michel">
+<img src="<?= i('nutshell/bretagne vs normandie.jpg') ?>" alt="Les bretons et les normands se disputant le Mont">
+<img src="<?= i('nutshell/salomon de bretagne.jpg') ?>" alt="Salomon de Bretagne">
+<img src="<?= i('nutshell/parallax/terres cotentin 1945.jpg') ?>" alt="Les terres du Cotentin en 1945">
+<img src="<?= i('nutshell/raoul roi des francs.jpg') ?>" alt="Raoul roi des francs">
+
 <?= t('nutshell.whatPurpose.question') ?>
 <?= t('nutshell.whatPurpose.answer') ?>
 <?= t('nutshell.whatPurpose.answer.more') ?>
 
+<img src="<?= i('nutshell/pelerin.png') ?>" alt="Un pelerin se rendant à St Jacques de Compostelle">
+<img src="<?= i('nutshell/prison.jpg') ?>" alt="Le Mont Saint-Michel servant de prison">
+<img src="<?= i('nutshell/louis xi.jpg') ?>" alt="Portrait de Louis XI">
+<img src="<?= i('nutshell/parallax/revolution fr.jpg', 'xlarge') ?>" alt="Prise de la Bastille lors de la révolution française">
+
 <?= t('nutshell.otherMonuments.question') ?>
-<?= t('nutshell.whatPurpose.answer') ?>
+<?= t('nutshell.otherMonuments.answer') ?>
 
 <?= t('nutshell.howLong.question') ?>
 <?= t('nutshell.howLong.answer') ?>
+
+<img src="<?= i('nutshell/dragon.jpg') ?>" alt="Combat entre l'archange St Michel et un dragon">
 
 <?php include "./includes/components/footer.php"; ?>
