883055e43dadd93c5fb7ab6f7717263fb0931929
AquaTuor <gfrancois2003@gmail.com>
2022-06-14 14:58:37 +0200

    Ajout de la page de présentation de l'équipe

---
+++ pages/team.php
@@ -0,0 +1,77 @@
+<?php
+include "./includes/components/navbar.php";
+?>
+
+<main>
+    <div class="team">
+        <div>
+        <h1>L'équipe</h1>
+        <p>Nous sommes une équipe constitué de cinq personnes. Nous avions chacun un rôle important à jouer au sein de l'équipe:</p>
+        </div>
+
+        <div class="Arno">
+            <h2> Arno </h2>
+            <h4>Chef de Projet</h4>
+            <ul>
+                <li> - Chef de l'équipe </li>
+                <li> - Designer du wireframing </li>
+                <li> - Développeur et designer du site principale </li>
+                <li> - Manager de la base de données </li>
+            </ul>
+        </div>
+
+        <br>
+
+        <div class="Yael">
+            <h2> Yael </h2>
+            <h4>Développeur & Traducteur</h4>
+            <ul>
+                <li> - Manager des traductions du site </li>
+                <li> - Traducteur anglais </li>
+                <li> - Community manager </li>
+                <li> - Développeur du site </li>
+                <li> - Designer du site </li>
+                <li> - Gestion de la base de données </li>
+            </ul>
+        </div>
+
+        <br>
+
+        <div class="Gabriel">
+            <h2> Gabriel </h2>
+            <h4>Développeur && Rechercheur</h4>
+            <ul>
+                <li> - Développeur du site </li>
+                <li> - Manager de la Bêta-Test</li>
+                <li> - Rechercheur des textes architecture et biodiversité </li>
+                <li> - Rechercheur des images </li>
+            </ul>
+        </div>
+
+        <br>
+
+        <div class="Clement">
+            <h2> Clément </h2>
+            <h4>Développeur && Rechercheur</h4>
+            <ul>
+                <li> - Développeur du site </li>
+                <li> - Rechercheur des textes histoire </li>
+                <li> - Gestion de la base de données </li>
+            </ul>
+        </div>
+
+        <br>
+
+        <div class="Christopher">
+            <h2> Christopher </h2>
+            <h4>Ancien Chef de Projet</h4>
+            <ul>
+                <li> - Développeur et designer du site </li>
+                <li> - Designer du wireframing </li>
+                <li> - Community manager </li>
+            </ul>
+        </div>
+    
+    </div>
+</main>
+<?php include "./includes/components/footer.php"; ?>
