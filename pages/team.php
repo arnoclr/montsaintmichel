fb00b9f31c437edf63ea4dbcf1874292cb2784bb
AquaTuor <gfrancois2003@gmail.com>
2022-06-14 15:52:09 +0200

    Fix page Team

--- pages/team.php
+++ pages/team.php
@@ -19,8 +19,8 @@
                 <li> - Manager de la base de données </li>
             </ul>
             <ul>
-                <li> <u><b>Mail :</b></u> <a href="mailto:bonjour@arnocellarier.fr"> bonjour@arnocellarier.fr </a> </li>
-                <li> <u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/arno-cellarier/" target="_blank"> linkedin.com/arno-cellarier </a> </li>
+                <li><u><b>Mail :</b></u><a href="mailto:msm@arnocellarier.fr"> msm@arnocellarier.fr</a></li>
+                <li><u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/arno-cellarier/" target="_blank"> linkedin.com/arno-cellarier</a></li>
             </ul>
                     
         </div>
@@ -29,9 +29,9 @@
 
         <div class="box-content">
             <h2> Yael </h2>
-            <h4>Développeur & Traducteur</h4>
+            <h4>Développeur et Traducteur</h4>
             <ul>
-                <li> - Manager des traductions du site </li>
+                <li> - En charge des traductions du site </li>
                 <li> - Traducteur anglais </li>
                 <li> - Community manager </li>
                 <li> - Développeur du site </li>
@@ -39,8 +39,8 @@
                 <li> - Gestion de la base de données </li>
             </ul>
             <ul>
-                <li> <u><b>Mail :</b></u> <a href="mailto:yael.coeffier@gmail.com"> yael.coeffier@gmail.com </a> </li>
-                <li> <u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/ya%C3%ABl-co%C3%ABffier-12a633227/" target="_blank"> linkedin.com/yaël-coëffier </a> </li>
+                <li><u><b>Mail :</b></u><a href="mailto:yael.coeffier+unesco@gmail.com"> yael.coeffier+unesco@gmail.com</a></li>
+                <li><u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/ya%C3%ABl-co%C3%ABffier-12a633227/" target="_blank"> linkedin.com/yaël-coëffier</a></li>
             </ul>
         </div>
 
@@ -56,8 +56,8 @@
                 <li> - En charge des des images </li>
             </ul>
             <ul>
-                <li> <u><b>Mail :</b></u> <a href="mailto:gfrancois2003@gmail.com"> gfrancois2003@gmail.com </a> </li>
-                <li> <u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/gabriel-fran%C3%A7ois-855988222/" target="_blank"> linkedin.com/gabriel-françois </a> </li>
+                <li><u><b>Mail :</b></u><a href="mailto:gfrancois2003+unesco@gmail.com"> gfrancois2003+unesco@gmail.com</a></li>
+                <li><u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/gabriel-fran%C3%A7ois-855988222/" target="_blank"> linkedin.com/gabriel-françois</a></li>
             </ul>
         </div>
 
@@ -72,8 +72,8 @@
                 <li> - Gestion de la base de données </li>
             </ul>
             <ul>
-                <li> <u><b>Mail :</b></u> <a href="mailto:clement.desimon@gmail.com"> clement.desimon@gmail.com </a> </li>
-                <li> <u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/cl%C3%A9ment-de-simon-71b632227/" target="_blank"> linkedin.com/clément-de-simon </a> </li>
+                <li><u><b>Mail :</b></u><a href="mailto:clement.desimon+unesco@gmail.com"> clement.desimon+unesco@gmail.com</a></li>
+                <li><u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/cl%C3%A9ment-de-simon-71b632227/" target="_blank"> linkedin.com/clément-de-simon</a></li>
             </ul>
         </div>
 
@@ -88,7 +88,7 @@
                 <li> - Community manager </li>
             </ul>
             <ul>
-                <li> <u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/christopher-beaurain-a464b0173/" target="_blank"> linkedin.com/christopher-beaurain </a> </li>
+                <li><u><b>LinkedIn</b></u> : <a href="https://www.linkedin.com/in/christopher-beaurain-a464b0173/" target="_blank"> linkedin.com/christopher-beaurain</a></li>
             </ul>
         </div>
     
