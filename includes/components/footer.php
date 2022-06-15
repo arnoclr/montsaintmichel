fc5306332a0359f43a4e91fd616056ae4d3569ed
Arno Cellarier <bonjour@arnocellarier.fr>
2022-06-15 08:29:53 +0200

    feat: ajout lien page remerciements

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -4,6 +4,7 @@
         <div class="footer__col col1">
             <h3 class="footer_section"><?= t('footer.title.link') ?></h3>
             <div class="footer__links">
+                <a href="/remerciements" class="footer__col-link"><?= t('footer.project.thanks') ?></a>
                 <a href="/equipe" class="footer__col-link"><?= t('footer.project.team') ?></a>
                 <a href="https://www.unesco.org" target="_blank" class="footer__col-link"><?= t('footer.project.website') ?></a>
                 <a href="https://www.univ-gustave-eiffel.fr/" target="_blank" class="footer__col-link"><?= t('footer.project.eiffel') ?></a>
